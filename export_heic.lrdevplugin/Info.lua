f1961ec9a06ee227c8e9e098df0b5ae1ca04b84e
Womabre <46841706+Womabre@users.noreply.github.com>
2025-09-17 21:51:56 +0200

    Update Info.lua

--- export_heic.lrdevplugin/Info.lua
+++ export_heic.lrdevplugin/Info.lua
@@ -2,10 +2,10 @@ return {
     LrSdkVersion = 10.0,
     LrSdkMinimumVersion = 10.0, 
     LrToolkitIdentifier = 'com.mxwu.heicexport',
-    LrPluginName = 'HEIC Exporter',
-    VERSION = { major=1, minor=0, revision=0, build=0, },
+    LrPluginName = 'HEIC / HDR Gain Map Exporter',
+    VERSION = { major=1, minor=1, revision=0, build=0, },
     LrExportServiceProvider = {
-        title = "Export to HEIC",
+        title = "Export to HEIC / HDR Gain Map",
         file = 'ExportServiceProvider.lua',
     },
 }
